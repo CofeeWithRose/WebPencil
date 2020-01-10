cee539ad0535ea8a55f76eabfadd7cd0b03910c6
CoffeeWithRose <1836071395@qq.com>
2020-01-10 09:42:48 +0800

    fix bug

---
+++ src/pages/paint/components/tools/color/ColorPannel.tsx
@@ -0,0 +1,8 @@
+import { ToolBarProps } from "../../toolbar";
+import React from 'react'
+
+export default ({ onSelectTool}: ToolBarProps)=>{
+    return <div>
+        colors...
+    </div>
+}
--- src/pages/paint/components/tools/color/index.tsx
+++ src/pages/paint/components/tools/color/index.tsx
@@ -1,25 +1,32 @@
 import { PainterTooolProps } from "../../pannel/interface";
-import React, { useState } from 'react'
+import React, { useState, Fragment } from 'react'
 import style from './index.less'
 import { Modal, Drawer } from 'antd'
+import ColorPannel from "./ColorPannel";
 
 export default ({onSelectTool, onActiveTool, curState}: PainterTooolProps)=> {
     const [color, setColor] = useState('#000000')
     const [showColorPanel, setShowColorPannel] = useState(false)
+    const handleShow = () => setShowColorPannel(show => !show)
     // const handleClick = () => {
     //     onActiveTool(ToolTypes.ERASER)
     //     onSelectTool(ToolTypes.ERASER, eraser)
     // }
     
-    return <div 
-        style={{backgroundColor: color}}
-        className={style.colorBar}
-        onClick={() => setShowColorPannel(show => !show)} 
-    >
-        <Drawer
-            visible={showColorPanel}
+    return <Fragment>
+        <div 
+            style={{backgroundColor: color}}
+            className={style.colorBar}
+            onClick={handleShow} 
         >
-
+        
+        </div>
+         <Drawer
+            visible={showColorPanel}
+            onClose={handleShow}
+        >
+            <ColorPannel onSelectTool={onSelectTool}/>
         </Drawer>
-    </div>
+    </Fragment>
+    
 }
