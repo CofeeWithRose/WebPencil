941016a11e1394ca81ef1ca5238a213e5ee72da5
CoffeeWithRose <1836071395@qq.com>
2020-01-23 14:09:16 +0800

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ ReactDom.render( <App/>, document.querySelector('#root') )
 
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('/service-worker.js').then(registration => {
+		navigator.serviceWorker.register('./service-worker.js').then(registration => {
 			console.log('SW registered: ', registration)
 		}).catch(registrationError => {
 			console.log('SW registration failed: ', registrationError)
