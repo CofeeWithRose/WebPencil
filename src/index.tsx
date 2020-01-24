8a16ac73665a45eb8612250a73ec0e0b084dde6f
CoffeeWithRose <1836071395@qq.com>
2020-01-24 23:52:49 +0800

    add regist error

--- src/index.tsx
+++ src/index.tsx
@@ -9,8 +9,8 @@ if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
 		navigator.serviceWorker.register('./service-worker.js').then(registration => {
 			console.log('SW registered: ', registration)
-		}).catch(registrationError => {
-			console.log('SW registration failed: ', registrationError)
+		}).catch((registrationError:Error) => {
+			console.log('SW registration failed: ', registrationError.message)
 		})
 	})
 }
