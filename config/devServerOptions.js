8394a828514d5ba4a357717307c9e70de986666d
mrp <1836071395@qq.com>
2020-04-17 16:40:55 +0800

    https

--- config/devServerOptions.js
+++ config/devServerOptions.js
@@ -1,6 +1,14 @@
+const fs = require('fs')
+const path = require('path')
+
 module.exports = {
   compress: true,
   port: 5000,
   hot: true,
   writeToDisk: true,
+  https: {
+    key: fs.readFileSync(path.resolve(__dirname, './cer/server_no_passwd.key')),
+    cert: fs.readFileSync(path.resolve(__dirname, './cer/server.crt')),
+    ca: fs.readFileSync(path.resolve(__dirname, './cer/server.csr')),
+  }
 }
