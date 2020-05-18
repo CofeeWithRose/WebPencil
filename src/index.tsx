ecf6e31914aa48f7874cab0ce1f766bf390db043
muyin <muyin.ph@alibaba-inc.com>
2020-05-18 20:43:24 +0800

    add visible log

--- src/index.tsx
+++ src/index.tsx
@@ -30,6 +30,6 @@ window.addEventListener('load', () => {
 		e.preventDefault()
 	},{passive:false,})
 })
-window.addEventListener('pageshow', () => {
-	console.log('pageshow')
+document.addEventListener('visibilitychange', (e) => {
+	console.log('focusin', document.visibilityState)
 })
