cf2a5df4f689495dd001d4a9c870d8c801ef1418
muyin <muyin.ph@alibaba-inc.com>
2020-06-30 10:47:43 +0800

    去除无用绘制

--- src/pages/paint/top-tool-bar/tool-item/brush/index.ts
+++ src/pages/paint/top-tool-bar/tool-item/brush/index.ts
@@ -66,8 +66,11 @@ export class Brush  implements AbstractBrush {
     onEnd(brushStatus: BrushStatus, {onBrushEnd}: PCanvasContext) {
     	requestAnimationFrame(() => {
     		this.isPainting = false
+    		if(this.pathList.length >1){
+    			onBrushEnd()
+    			console.log('onBrushEnd: ', this.pathList.length)
+    		}
     		this.pathList = []
-    		onBrushEnd()
     	})
     }
 
