91aa15eee884146fe922418c9984f781edaf6fc0
PH <1836071395@qq.com>
2020-07-05 19:43:40 +0800

    update

--- src/pages/paint/top-tool-bar/brush/new.brush.tsx
+++ src/pages/paint/top-tool-bar/brush/new.brush.tsx
@@ -3,19 +3,20 @@ import { Vector2 } from '../../../../util/data/Vector2'
 import { BrushType, BrushStatus, AbstractBrush } from './types'
 import VConsole from 'vconsole'
 
-class BrushPathPoint { 
-	constructor(
-    public readonly center: Vector2,
-		public readonly right: Vector2,
-    public readonly left: Vector2,
+// class BrushPathPoint { 
+// 	constructor(
+//     public readonly center: Vector2,
+// 	public readonly right: Vector2,
+//     public readonly left: Vector2,
 
-		// public readonly normalizedDirection: Vector2,
-		// /**
-		//  * 与上个方向的夹角是否为锐角.
-		//  */
-		// public readonly isSharp: boolean
-	){}
-}
+// 		// public readonly normalizedDirection: Vector2,
+// 		// /**
+// 		//  * 与上个方向的夹角是否为锐角.
+// 		//  */
+// 		// public readonly isSharp: boolean
+// 	){}
+// }
+type PathInfo = {right: Vector2[], left: Vector2[], angles: [ Vector2, Vector2, Vector2][]} 
 /**
  * 带压感的brush，bug待修复.
  * 根据移动，与context, 控制画出的形状并填充.
@@ -26,13 +27,15 @@ export class Brush  implements AbstractBrush {
 
     name = 'new'
 
-    isPainting = false
+	isPainting = false
+	
+	protected isFirst = false
 
     protected lastBrushStatus: BrushStatus|null
 
-    protected lastBrushPathPoint: BrushPathPoint
+    // protected lastBrushPathPoint: BrushPathPoint
 
-    protected brushPointList: BrushPathPoint[] = []
+    protected pathInfo: PathInfo = { right:[], left: [], angles: []  }
 
     init(){
 
@@ -46,6 +49,7 @@ export class Brush  implements AbstractBrush {
     	const {curCanvasContext2D: ctx, color} = context
     	this. lastBrushStatus = status
     	this.isPainting = true
+    	this.isFirst = true
     	ctx.fillStyle = color
     }
 
@@ -68,64 +72,71 @@ export class Brush  implements AbstractBrush {
     		)
     		const normalizedDirection = Vector2.normalize(direction)
     		const rightDirection = Vector2.multipy(Vector2.vertical(normalizedDirection), halfWidth) as Vector2 
-    		let right = Vector2.add( curStatus,  rightDirection)
-    		let left  = Vector2.subtract( curStatus, rightDirection )
-        
-    		if(!this.lastBrushPathPoint){
+    		let rightPoint:Vector2 = Vector2.add( curStatus,  rightDirection)
+    		let leftPoint:Vector2  = Vector2.subtract( curStatus, rightDirection )
+    		const { angles, left, right } = this.pathInfo
+    		if(this.isFirst){
     			// 第一个点.
-    			// const isSharp = false
-    			this.lastBrushPathPoint = new BrushPathPoint( 
-    				this.lastBrushStatus,
-    				Vector2.add(this.lastBrushStatus, rightDirection),
-    				Vector2.subtract( this.lastBrushStatus, rightDirection),
-    			)
-    			this.brushPointList.push(this.lastBrushPathPoint)
+    			const rightPoint0 = Vector2.add(this.lastBrushStatus, rightDirection)
+    			const leftPoint0 = Vector2.subtract( this.lastBrushStatus, rightDirection)
+    			this.pathInfo.right.push(rightPoint0)
+    			this.pathInfo.left.push(leftPoint0)
     			// 第二个点.
-    			this.lastBrushPathPoint = new BrushPathPoint( curStatus,right,left)
+    			this.pathInfo.right.push(rightPoint)
+    			this.pathInfo.left.push(leftPoint)
+    			this.pathInfo.angles.push([
+    				rightPoint0, rightPoint, leftPoint0
+    			])
+    			this.pathInfo.angles.push([
+    				leftPoint0, leftPoint, rightPoint, 
+    			])
+    			this.isFirst = false
     			this.lastBrushStatus = curStatus
-    			this.brushPointList.push(this.lastBrushPathPoint)
     			break
     		}
-    		// const multipy: number = Vector2.multipy(normalizedDirection, this.lastBrushPathPoint.normalizedDirection) as number 
-    		// const isSharp = (1- multipy) < 1 * 10 ** -15   && multipy > 0
-    		const isRightIn = this.isPointInPath(left)
-    		const isLeftIn = this.isPointInPath(right)
-    		if(isRightIn && isLeftIn){
-    			break
-    		}else if(isRightIn){
-    			right = this.getCorssPoint(right, left)
-    		}else if(isLeftIn) {
-    			left = this.getCorssPoint(right, left)
+    		const isRightIn = this.isPointInPath(rightPoint)
+    		const isLeftIn = this.isPointInPath(leftPoint)
+    		if(isRightIn){
+    			rightPoint = this.getCorssPoint(rightPoint, leftPoint)
     		}
-    		this.lastBrushPathPoint =new BrushPathPoint( curStatus, right, left )
+    		if(!isLeftIn){
+    			leftPoint = this.getCorssPoint(rightPoint, leftPoint)
+    		}
+    		this.pathInfo.right.push(rightPoint)
+    			angles.push([
+    				left[left.length-1],
+    				right[right.length-1],
+    				right[right.length - 2 ]
+    		])
+    		this.pathInfo.left.push(leftPoint)
+    			angles.push([
+    				right[right.length-1],
+    				left[left.length-1],
+    				left[left.length-2],
+    			])
+    		
     		this.lastBrushStatus = curStatus
-    		this.brushPointList.push(this.lastBrushPathPoint)
     	}
     	this.fillPath(ctx)
     }
 
+    /**
+	 * 判断点是否在当前路径下.
+	 * @param v 
+	 */
     protected isPointInPath = (v: Vector2) => {
-    	let count = 0
-    	const {right: startRight, left: startLeft} = this.brushPointList[0]
-    	if(this.isPointDownLineCrossLine(v, {p1:startRight, p2: startLeft })){
-    		count++
-    	}
-    	const {right: endRight, left: endLeft} = this.brushPointList[this.brushPointList.length-1]
-    	if(this.isPointDownLineCrossLine(v, {p1: endRight, p2: endLeft })){
-    		count++
-    	}
-    	for(let i =0; i< this.brushPointList.length -1; i++){
-    		const {right, left} = this.brushPointList[i]
-    		const {right:nextRight, left: nextLeft} = this.brushPointList[i+1]
-    		if(this.isPointDownLineCrossLine(v, {p1:right, p2: nextRight })){
-    			count++
-    		}
-    		if(this.isPointDownLineCrossLine(v, {p1:left, p2: nextLeft })){
-    			count++
+    	const { angles } = this.pathInfo
+    	for(let i = 0; i< angles.length; i++){
+    		const angle = angles[i]
+    		let count = 0
+    		if(this.isPointDownLineCrossLine(v, {p1: angle[0], p2: angle[1] })) count++
+    		if(this.isPointDownLineCrossLine(v, {p1: angle[0], p2: angle[2] })) count++
+    		if(this.isPointDownLineCrossLine(v, {p1: angle[1], p2: angle[2] })) count++
+    		if(count %2 ===1 ){
+    			return true
     		}
     	}
-    	// console.log(count)
-    	return count %2 === 1
+    	return false
     }
 
     /**
@@ -144,7 +155,7 @@ export class Brush  implements AbstractBrush {
     		return false
     	}
     	const crosY = this.getY(p.x, {p1,p2})
-    	if(crosY > p.y){
+    	if(crosY >= p.y){
     		return true
     	}
     	return false
@@ -162,44 +173,67 @@ export class Brush  implements AbstractBrush {
     	return k * x+b
     }
     
-    protected getCorssPoint = (left: Vector2, right: Vector2) => {
-    	const {k,b} = this.getKb(left, right)
-    	for( let i =0; i< this.brushPointList.length -1; i++){
-    		const {k: k1, b: b1} = this.getKb(this.brushPointList[i].left, this.brushPointList[i+1].left )
+    protected getCorssPoint = (p1: Vector2, p2: Vector2) => {
+    	const {k,b} = this.getKb(p1, p2)
+    	let maxX: number = p1.x > p2.x?  p1.x: p2.x
+    	let minX: number = p1.x <= p2.x?   p1.x: p2.x
+    	const line = { maxX, minX, k, b }
+    	let v : Vector2 |undefined
+    	const { angles } = this.pathInfo
+    	for(let i = 0; i< angles.length; i++){
+    		const angle = angles[i]
+    		let c: Vector2|null
+    		c =  this.getCross({maxX, minX,k, b, p1, p2}, { p1: angle[0], p2: angle[1]})
+    		if(c) return c
+    		c =  this.getCross({maxX, minX,k, b, p1, p2}, { p1: angle[0], p2: angle[2]})
+    		if(c) return c
+    		c =  this.getCross({maxX, minX,k, b, p1, p2}, { p1: angle[2], p2: angle[1]})
+    		if(c) return c
     	}
-    	this.brushPointList
-    	//   y=kx+b
-    	//   y=k2x+b2
-    	//   0=(k2-k)x +b2-b
-    	//  x0=b-b2/k2-k
-    	//  x0=b-b2/()
-
     	return Vector2.ZERO
     }
-    
+	
+	protected getCross = (line1: {maxX: number, minX: number, k: number, b: number, p1:Vector2, p2:Vector2}, line2:{p1:Vector2, p2:Vector2}) => {
+		const {k: k1, b: b1} = this.getKb(line2.p1, line2.p2 )
+		// if(isNaN(line1.k)&& isNaN(k1)){
+		// 	return line1.
+		// }
+		const x = (line1.b-b1)/(k1-line1.k)
+		const max2 = line2.p1.x > line2.p2.x? line2.p1.x: line2.p2.x
+		const min2 = line2.p1.x <= line2.p2.x? line2.p1.x: line2.p2.x
+		const max = Math.min(max2, line1.maxX)
+		const min = Math.max(min2, line1.minX )
+		if(x >= min && x <= max ){
+			const c = new Vector2( x, k1*x + b1 )
+			return c
+		}
+		return null
+	}
+ 
     protected getKb = (v1: Vector2, v2:Vector2) => {
     	const k = (v1.y-v2.y)/v1.x-v2.x
     	const b = v1.y - k * v1.x
+    	if(isNaN(k)){
+    		console.log('getKb: ',v1, v2)
+    		throw '123'
+    	}
     	return { k, b }
     }
 
     protected fillPath(ctx: CanvasRenderingContext2D){
+    	const { right, left } = this.pathInfo
     	ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height)
+    	
+		
     	ctx.beginPath()
-    	this.brushPointList.forEach(({  right, left }, ind) => {
-    		if(ind ===0){
-    			ctx.moveTo(left .x, left.y)
-    		}else{
-    			const p = right
-    			// const p = isSharp? right:  left
-    			ctx.lineTo(p.x, p.y)
-    		}
-    	})
-    	for( let i = this.brushPointList.length -1; i >=0; i--){
-    		const { left, right} = this.brushPointList[i]
-    		// const p =  isSharp? left : right
-    	  const	p = left
-    		ctx.lineTo(p.x, p.y)
+    	ctx.moveTo( left[0].x, left[0].y )
+    	for(let i = 1; i< left.length; i++){
+    		ctx.lineTo(left[i].x, left[i].y)
+    		// ctx.fillRect(left[i].x, left[i].y, 1, 1)
+    	}
+    	for(let i = right.length -1; i>= 0; i--){
+    		ctx.lineTo(right[i].x, right[i].y)
+    		// ctx.fillRect(right[i].x, right[i].y, 1, 1)
     	}
     	ctx.closePath()
     	ctx.stroke()
@@ -208,7 +242,7 @@ export class Brush  implements AbstractBrush {
     onEnd(brushStatus: BrushStatus, {onBrushEnd}: PCanvasContext) {
     	this.isPainting = false
     	this.lastBrushStatus = null
-    	this.brushPointList = []
+    	this.pathInfo = { left:[], right:[], angles:[] }
     	onBrushEnd()
     }
 
